07ee418f84498a39d46b1979e9407e10e4d512d3
Janice <janice.c.chung@gmail.com>
2019-08-27 21:29:53 +1200

    ugh

---
+++ tasks.js
@@ -0,0 +1,10 @@
+var tasks = [
+
+"Flute Exam - Thursday 29th August, 2:45 pm",
+"WYS Salvation Army Concert - Saturday 31st August, 9:30 am to 12:00 pm, 4:00 pm. Bring music stand, wear white top black bottom."
+
+]
+
+name
+date
+
